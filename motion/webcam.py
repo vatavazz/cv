35c50805d04a19050790e0356a089a8f53bdb120
vatavazz <vatavazz@users.noreply.github.com>
2017-08-15 20:35:52 +0200

    organised

--- motion/webcam.py
+++ motion/webcam.py
@@ -2,19 +2,15 @@ import numpy as np
 import cv2
 import time
 from datetime import datetime
-from facedetector import FaceDetector
 
 # in seconds
 VID_LENGTH = 10
 MIN_AREA = 250
 FOURCC = cv2.VideoWriter_fourcc(*'XVID')
-fd = FaceDetector("haarcascade_frontalface_default.xml")
 
 cap = cv2.VideoCapture(0)
 
-
 concluded = False
-# firstFrame = cv2.imread("background.jpg")
 firstFrame = None
 startTime = None
 
@@ -27,13 +23,6 @@ while True:
 	
 	if firstFrame is None:
 		firstFrame = blur
-	
-	# face detetcion
-	# faces = frame
-	# faceRects = fd.detect(grey, scaleFactor = 1.1, minNeighbors = 5, minSize = (30,30))
-	# for (x,y,w,h) in faceRects:
-		# cv2.rectangle(faces, (x,y), (x+w, y+h), (0, 255, 0), 2)
-	# cv2.imshow("faces video", faces)
 	
 	# motion detection
 	frameDelta = cv2.absdiff(firstFrame, blur)
@@ -69,10 +58,6 @@ while True:
 	
 	
 	k=cv2.waitKey(10)& 0xff
-	# if k == 32:
-		# capname = "{}.jpg".format(str(datetime.now().isoformat()))
-		# capname = capname.replace(":","-")
-		# cv2.imwrite(capname, blur)
 	if k == 27:
 		break
 
