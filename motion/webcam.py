e2f8478ed0f7e04e39890fa89a1e3034833d78e0
vatavazz <vatavazz@users.noreply.github.com>
2017-08-17 18:12:21 +0200

    prettier

--- motion/webcam.py
+++ motion/webcam.py
@@ -5,7 +5,9 @@ from datetime import datetime
 
 # in seconds
 VID_LENGTH = 10
+# size of detected motion; experiment
 MIN_AREA = 250
+# codec
 FOURCC = cv2.VideoWriter_fourcc(*'XVID')
 
 cap = cv2.VideoCapture(0)
@@ -13,8 +15,6 @@ cap = cv2.VideoCapture(0)
 concluded = False
 firstFrame = None
 startTime = None
-
-LOOPS = 5
 
 while True:
 	ret, frame = cap.read()
@@ -29,20 +29,19 @@ while True:
 	thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 	thresh = cv2.dilate(thresh, None, iterations=2)
 	diff = cv2.subtract(firstFrame, blur)
+	# win fix, experiment
 	_, cnts, _= cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	
 	for c in cnts:
-		if cv2.contourArea(c) < MIN_AREA:
-			continue
-		
-		(x,y,w,h) = cv2.boundingRect(c)
-		cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+		if cv2.contourArea(c) >= MIN_AREA:
+			(x,y,w,h) = cv2.boundingRect(c)
+			cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
 	
-	cv2.imshow("motion video", frame)
+	cv2.imshow("Video Feed", frame)
 	
 	if (len(cnts) > 0) and (startTime == None):
 		startTime = time.time()
-		print("detected motion, saving to file")
+		print("detected motion, recording video")
 		capname = "{}.avi".format(str(datetime.now().isoformat()))
 		capname = capname.replace(":","-")
 		vid = cv2.VideoWriter(capname, FOURCC, 5, (640, 480))
@@ -56,12 +55,11 @@ while True:
 			concluded = True
 			startTime = None
 	
-	
 	k=cv2.waitKey(10)& 0xff
 	if k == 27:
 		break
 
-cap.release()
 if vid != None:
 	vid.release()
+cap.release()
 cv2.destroyAllWindows()
